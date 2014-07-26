4716510a35a65b4713fc5a62abfdc4502a68a30d
Karol Stępniewski <karol.stepniewski@gmail.com>
2014-07-26 16:09:37 +0200

    initial information changes after fork

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 Redmine::Plugin.register :redmine_keyboard_shortcuts do
   name 'Redmine Keyboard Shortcuts'
-  author 'Austin Smith'
-  description 'Add vim-style keyboard shortcuts to Redmine'
-  version '0.0.1'
-  url 'https://github.com/netaustin/redmine_keyboard_shortcuts'
-  author_url 'http://www.alleyinteractive.com/'
+  author 'Austin Smith (modified by Karol Stępniewski)'
+  description 'Add keyboard shortcuts to Redmine'
+  version '0.0.2'
+  url 'https://github.com/kars7e/redmine_keyboard_shortcuts'
+  author_url 'http://blog.bigfun.eu'
 end
 
 require 'redmine_keyboard_shortcuts/hooks.rb'
