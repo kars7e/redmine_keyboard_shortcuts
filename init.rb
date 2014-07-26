c294f40ec135374704f11f962c73ff01879ee17e
Karol Stępniewski <karol.stepniewski@gmail.com>
2014-07-26 17:48:59 +0200

    fixed polish letter which caused entire redmine to crash

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 Redmine::Plugin.register :redmine_keyboard_shortcuts do
   name 'Redmine Keyboard Shortcuts'
-  author 'Austin Smith (modified by Karol Stępniewski)'
+  author 'Austin Smith (modified by Karol Stepniewski)'
   description 'Add keyboard shortcuts to Redmine'
   version '0.0.2'
   url 'https://github.com/kars7e/redmine_keyboard_shortcuts'
